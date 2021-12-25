4d20c54d164af8b4af33c74ff5ca7625a2a21167
kinggo1102 <95344029+kinggo1102@users.noreply.github.com>
2021-12-26 00:15:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,24 +40,21 @@ MODEL_PATH = 'models/my_model.h5'
 #Load your own trained model
 #model = load_model(MODEL_PATH)
 model = load_model('MODEL_PATH')
-model._make_predict_function()          # Necessary
+# model._make_predict_function()          # Necessary
 print('Model loaded. Start serving...')
 
 
 def model_predict(img, model):
-    img = img.resize((100, 100))
+    img = img.resize((256, 256))
 
     # Preprocessing the image
     x = image.img_to_array(img)
     # x = np.true_divide(x, 255)
-    x = np.expand_dims(x, axis=0)
-
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    x = preprocess_input(x, mode='tf')
-
-    preds = model.predict(x)
-    return preds
+    img = np.reshape(img,[1,256,256,3])
+    classes = model.predict(img)
+    result = np.argmax(classes)
+    
+    return result
 
 
 @app.route('/', methods=['GET'])
@@ -77,16 +74,16 @@ def predict():
 
         # Make prediction
         preds = model_predict(img, model)
+        ar = ['braeburn apple', 'pink lady apple', 'unqualified apple']
+#         # Process your result for human
+#         pred_proba = "{:.3f}".format(np.amax(preds))    # Max probability
+#         pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
 
-        # Process your result for human
-        pred_proba = "{:.3f}".format(np.amax(preds))    # Max probability
-        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-
-        result = str(pred_class[0][0][1])               # Convert to string
-        result = result.replace('_', ' ').capitalize()
-        
+#         result = str(pred_class[0][0][1])               # Convert to string
+#         result = result.replace('_', ' ').capitalize()
+        result = ar[int(preds)]
         # Serialize the result, you can add additional fields
-        return jsonify(result=result, probability=pred_proba)
+        return jsonify(result=result, probability=10)
 
     return None
 
